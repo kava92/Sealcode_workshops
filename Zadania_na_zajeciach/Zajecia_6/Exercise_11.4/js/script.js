aab645cba971cf7dde5a2f1fea35835496345795
Pawel <kava92@github.com>
2017-12-11 16:33:18 +0100

    Add Exercise_11.4

---
+++ Zadania_na_zajeciach/Zajecia_6/Exercise_11.4/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var p1 = document.getElementById("paragraph");
+    p1.setAttribute("class", "akapit");
+    p1.setAttribute("id", "zmieniony");
+  });
